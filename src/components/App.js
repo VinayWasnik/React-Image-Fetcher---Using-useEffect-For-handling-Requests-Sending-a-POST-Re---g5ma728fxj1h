3f3b4d9c09f83c0bf1f38c49cd35f0766645ff31
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-01 10:56:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,4 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
-const App = () => {
-  const [inputNumber, setInputNumber] = useState('');
-  const [photoData, setPhotoData] = useState(null);
-  cost [isLoading, setIsLoading] = useState(false);
-
-  const handleInputChange = (event) => {
-    setInputNumber(event.target.value);import React, { useState } from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
@@ -23,6 +13,8 @@ const App = () => {
   };
 
   const fetchData = async () => {
+    if (!inputNumber) return;
+
     try {
       setIsLoading(true);
       const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${inputNumber}`);
@@ -39,7 +31,6 @@ const App = () => {
   };
 
   const handleSearch = () => {
-    if (!inputNumber) return;
     fetchData();
   };
 
@@ -51,61 +42,13 @@ const App = () => {
         onChange={handleInputChange}
         placeholder="Enter a number between 1 and 5000"
       />
-
       <button onClick={handleSearch}>Search</button>
 
       {isLoading && <Loader />}
 
-      {photoData && (
-        <PhotoFrame url={photoData.url} title={photoData.title} />
-      )}
+      {photoData && <PhotoFrame url={photoData.url} title={photoData.title} />}
     </div>
   );
 };
-
-export default App;
-
-};
-
-const fetchData = () => {
-  setIsLoading(true);
-  fetch(`https://jsonplaceholder.typicode.com/photos/${inputNumber}`)
-  .then((response) => response.json())
-  .then((data) => {
-    setIsLoading(false);
-    setPhotoData(data);
-  })
-  .catch((error) => {
-    setIsLoading(false);
-    console.error('Error fetching data:', error);
-  });
-};
-
-const handleSearch = () => {
-  if(!inputNumber) return;
-  fetchData();
-};
-
-return (
-  <div>
-  <input
-  type="number"
-    value={inputNumber}
-      onChange={handleInputChange}
-        placeholder="Enter a number between 1 and 5000"
-          />
-          
-          <button onClick={handleSearch}>Search</button>
-  
-  {isLoading && <Loader />}
-
-  {photoData && (
-    <PhotoFrame url={photoData.url} title={photoData.title} />
-  )}
-  </div>
-);
-};
-
-
 
 export default App;
