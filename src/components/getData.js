f2d366b7e9f114719d0ecfe7346233efb9ed9da2
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 16:50:04 +0530

    Delete getData.js

--- src/components/getData.js
+++
@@ -1,6 +0,0 @@
-export const getData = async () => {
-    // This function simulates an asynchronous operation by sleeping for 2 seconds.
-    await new Promise((resolve, reject) => setTimeout(resolve, 2000));
-    return "Hello, world!";
-  };
-  
